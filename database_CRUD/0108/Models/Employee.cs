fa3c07e5a11452f4e4328bf27cf9c830b90429b8
Rein0527 <108525337+Rein0527@users.noreply.github.com>
2023-01-12 16:33:20 +0800

    Add files via upload

---
+++ database_CRUD/0108/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace _0108.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string EmployeeName { get; set; } = null!;
+        public string FatheName { get; set; } = null!;
+    }
+}
